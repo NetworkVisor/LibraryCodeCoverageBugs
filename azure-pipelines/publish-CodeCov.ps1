f6ca2f998e7a93a3b7fa2144bed8a8f7c705a5d3
Steve Bush <stevebu@bushchang.com>
2022-06-14 07:32:14 -0700

    Update publish-CodeCov.ps1

--- azure-pipelines/publish-CodeCov.ps1
+++ azure-pipelines/publish-CodeCov.ps1
@@ -16,6 +16,13 @@ $codeCovToolPath = & "$PSScriptRoot/Get-CodeCovTool.ps1"
 Write-Host "Path to codecov $codeCovToolPath" -ForegroundColor Yellow
 
 Get-ChildItem -Recurse -Path "$PathToCodeCoverage/*.cobertura.xml" | % {
+
+    # Replace Directory Separator on Windows
+    if (-not ($IsLinux -or $IsMacOS))
+    {
+        $_.replace([IO.Path]::DirectorySeparatorChar, [IO.Path]::AltDirectorySeparatorChar)
+    }
+
     Write-Host "Uploading $_" -ForegroundColor Yellow
     & (& "$PSScriptRoot/Get-CodeCovTool.ps1") -t $CodeCovToken -f $_
 }
