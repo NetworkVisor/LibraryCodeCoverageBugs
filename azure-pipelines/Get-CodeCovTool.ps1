2851557881dd76ae2b6923aacb19de275ce01ffa
Steve Bush <stevebu@bushchang.com>
2022-06-13 20:02:42 -0700

    Support for downloading Code Coverage Tool

---
+++ azure-pipelines/Get-CodeCovTool.ps1
@@ -0,0 +1,34 @@
+<#
+.SYNOPSIS
+    Downloads the CodeCov.io uploader tool and returns the path to it.
+#>
+[CmdletBinding()]
+Param(
+)
+
+$toolsPath = & "$PSScriptRoot\Get-TempToolsPath.ps1"
+$binaryToolsPath = Join-Path $toolsPath codecov
+if (!(Test-Path $binaryToolsPath)) { $null = mkdir $binaryToolsPath }
+
+if ($IsMacOS)
+{
+    $codeCovPath = Join-Path $binaryToolsPath codecov
+    $codeCovUrl = "https://uploader.codecov.io/latest/macos/codecov"
+}
+else if ($IsLinux)
+{
+    $codeCovPath = Join-Path $binaryToolsPath codecov
+    $codeCovUrl = "https://uploader.codecov.io/latest/linux/codecov"
+}
+else
+{
+    $codeCovPath = Join-Path $binaryToolsPath codecov.exe
+    $codeCovUrl = "https://uploader.codecov.io/latest/windows/codecov.exe"
+}
+
+if (!(Test-Path $codeCovPath)) {
+    Write-Host "Downloading latest codeconv..." -ForegroundColor Yellow
+    (New-Object System.Net.WebClient).DownloadFile($codeCovUrl, $codeCovPath)
+}
+
+return (Resolve-Path $codeCovPath).Path
