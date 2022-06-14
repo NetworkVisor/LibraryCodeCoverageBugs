4d558c6f7d937801c359e43ec750ea8f2d64c70a
Steve Bush <stevebu@bushchang.com>
2022-06-14 16:22:29 -0700

    Update publish-CodeCov.ps1

--- azure-pipelines/publish-CodeCov.ps1
+++ azure-pipelines/publish-CodeCov.ps1
@@ -18,7 +18,7 @@ $CodeCoveragePathWildcard = (Join-Path $PathToCodeCoverage "*.cobertura.xml")
 Write-Host "RepoRoot: $RepoRoot" -ForegroundColor Yellow
 Write-Host "CodeCoveragePathWildcard: $CodeCoveragePathWildcard" -ForegroundColor Yellow
 
-$Inputs = [string]::join(',', (Get-ChildItem -Recurse -Path $CodeCoveragePathWildcard | %
+$Inputs = [string]::join(',', (Get-ChildItem -Recurse -Path $CodeCoveragePathWildcard |%
 {
     if ($IsMacOS -or $IsLinux)
     {
